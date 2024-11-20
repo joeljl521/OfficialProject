6c60b835f147fe9707fddc9fd08a5cf0e68f9627
knigh <Saraj15@farmingdale.edu>
2024-11-20 15:56:53 -0500

    updated

--- src/main/java/joel/officialproject/HelloApplication.java
+++ src/main/java/joel/officialproject/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     public static Scene scene;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("useraccountinfo.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainmenu.fxml"));
         scene = new Scene(fxmlLoader.load(), 335, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
